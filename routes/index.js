d3d3ae42a9bcc3838a60c2d4a7681d12675d066c
Ridwan Abiola <ricklick262@gmail.com>
2022-12-14 16:22:00 +0100

    fix: endpoint

--- routes/index.js
+++ routes/index.js
@@ -6,31 +6,7 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 router.get("/test", (req, res) => {
-  res.send("Workin...");
+  res.send("Working...");
 });
 
-function isAuth(req, res, next) {
-  const auth = req.headers.authorization;
-  if (auth === "password") {
-    next();
-  } else {
-    res.status(401);
-    res.send("Access forbidden");
-  }
-}
-
-router.get("/secrets", isAuth, (req, res) => {
-  const secrets = [
-    {
-      id: 1,
-      name: "Secret 1",
-    },
-    {
-      id: 2,
-      name: "Secret",
-    },
-  ];
-
-  res.json(secrets);
-});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,8 @@
 var express = require("express");
-const { isAuth } = require("../utils/auth");
 var router = express.Router();
 
 /* GET users listing. */
 router.get("/", async function (req, res) {
-  console.log("$$////");
   const {
     sort_field = "firstname",
     sort_order_mode = "asc",
@@ -15,38 +13,44 @@ router.get("/", async function (req, res) {
   } = req.query;
   const startIndex = (parseInt(page) - 1) * page_size;
   const endIndex = page * page_size;
-
   try {
     const message = await req.context.models.User.find({
       [filter_field]: [filter_value],
     })
       .sort({
-        [sort_field]: [sort_order_mode],
+        [sort_field]: [sort_order_mode] || -1,
       })
       .limit(parseInt(page_size))
       .skip(startIndex);
     res.status(200).json(message);
   } catch (error) {
     console.error(error);
+    res.status(500).send("Internal server error");
   }
 });
+
 router.get("/:id", async function (req, res, _) {
   const message = await req.context.models.User.find({
     id: req.params.id,
   });
+  console.log("$$$: ", message);
   res.status(200).json(message);
 });
 
 router.post("/", async (req, res) => {
   const { firstname, lastname, gender, date_of_birth } = req.body;
 
-  const message = await req.context.models.User.create({
-    firstname,
-    lastname,
-    gender,
-    date_of_birth,
-  });
-  res.status(200).json({ error: false, message });
+  try {
+    const message = await req.context.models.User.create({
+      firstname,
+      lastname,
+      gender,
+      date_of_birth,
+    });
+    res.status(200).json(message);
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
 });
 
 router.put("/:id", async (req, res) => {
@@ -61,6 +65,9 @@ router.put("/:id", async (req, res) => {
     data,
     { new: true }
   );
+  if (!user) {
+    return res.status(400).send("User not found");
+  }
   res.status(200).send(user);
 });
 
@@ -68,24 +75,12 @@ router.delete("/:id", async (req, res) => {
   const { User } = req.context.models;
   const { id } = req.params;
 
-  await User.deleteOne({
+  const user = await User.deleteOne({
     id,
   });
-  res.send(200);
+  if (user.deletedCount === 0)
+    return res.status(400).send("User does not exist");
+  res.sendStatus(200);
 });
 
-router.get("/secrets", (req, res) => {
-  const secrets = [
-    {
-      id: 1,
-      name: "Secret 1",
-    },
-    {
-      id: 2,
-      name: "Secret",
-    },
-  ];
-
-  res.json(secrets);
-});
 module.exports = router;
